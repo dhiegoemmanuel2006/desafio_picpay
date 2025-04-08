5e97f06e263329332bb4660928929ba572234427
dhiego <dhiego12052006@gmail.com>
2025-04-08 01:10:41 -0300

    update: arrumando campo para se adequar ao banco de dados

--- src/main/java/desafio_backend_picpay/desafio/database/repositories/UserRepository.java
+++ src/main/java/desafio_backend_picpay/desafio/database/repositories/UserRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<Users, Long> {
-    Optional<Users> findByCpfOrCnpj(String cpfOrCnpj);
+    Optional<Users> findByCpf(String cpf);
 
     Optional<Users> findByEmail(String email);
 }
