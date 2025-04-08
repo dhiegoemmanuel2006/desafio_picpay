cbb9902e1cf160e1ce2a83826a292aa689187cb9
dhiego <dhiego12052006@gmail.com>
2025-04-08 00:27:54 -0300

    add: UsersController

---
+++ src/main/java/desafio_backend_picpay/desafio/controllers/UsersController.java
@@ -0,0 +1,23 @@
+package desafio_backend_picpay.desafio.controllers;
+
+import desafio_backend_picpay.desafio.dtos.CreateUserDTO;
+import desafio_backend_picpay.desafio.services.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/users")
+@RequiredArgsConstructor
+public class UsersController {
+    private final UserService uService;
+
+    @PostMapping
+    public ResponseEntity<Void> createUser(@RequestBody CreateUserDTO dto) throws Exception{
+        uService.createUser(dto);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+}
