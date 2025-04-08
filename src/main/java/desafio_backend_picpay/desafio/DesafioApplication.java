1603468d4cdddf882e5f71cf43ec6005515fd818
dhiego <dhiego12052006@gmail.com>
2025-04-08 01:09:19 -0300

    Ativando o Feign no projeto

--- src/main/java/desafio_backend_picpay/desafio/DesafioApplication.java
+++ src/main/java/desafio_backend_picpay/desafio/DesafioApplication.java
@@ -2,7 +2,9 @@ package desafio_backend_picpay.desafio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class DesafioApplication {
 
