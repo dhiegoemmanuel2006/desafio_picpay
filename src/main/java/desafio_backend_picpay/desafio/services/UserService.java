b514625e3dfbce63e073679b01b738a75d8e4e98
dhiego <dhiego12052006@gmail.com>
2025-04-08 00:28:21 -0300

    add: Funcionalidades do serviços dos users, criação de usuários, validações e etc.

---
+++ src/main/java/desafio_backend_picpay/desafio/services/UserService.java
@@ -0,0 +1,45 @@
+package desafio_backend_picpay.desafio.services;
+
+import desafio_backend_picpay.desafio.database.entities.Users;
+import desafio_backend_picpay.desafio.database.repositories.UserRepository;
+import desafio_backend_picpay.desafio.dtos.CreateUserDTO;
+import desafio_backend_picpay.desafio.handler.exceptions.CpfOrCnpjAlreadyExistsException;
+import desafio_backend_picpay.desafio.handler.exceptions.EmailAlreadyExistsException;
+import desafio_backend_picpay.desafio.handler.exceptions.NotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+    private final UserRepository uRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final UserRepository userRepository;
+
+    public void createUser(CreateUserDTO request) throws EmailAlreadyExistsException, CpfOrCnpjAlreadyExistsException {
+        validateEmailAndCPF(request);
+        userRepository.save(Users.builder().name(request.getName())
+                .type(request.getType())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .CpfOrCnpj(request.getCpfOrCnpj())
+                .balance(request.getBalance()).build());
+    }
+
+
+    public Users findById(Long id) throws NotFoundException {
+        return uRepository.findById(id)
+                .orElseThrow(()-> new NotFoundException(String.format("Usuário nao encontrado: %s", id)));
+    }
+    public void save(Users user){
+        uRepository.save(user);
+    }
+    public void validateEmailAndCPF(CreateUserDTO request) throws CpfOrCnpjAlreadyExistsException, EmailAlreadyExistsException {
+        Users user = uRepository.findByCpfOrCnpj(request.getCpfOrCnpj()).orElse(null);
+        if(user != null)throw new CpfOrCnpjAlreadyExistsException(request.getCpfOrCnpj());
+
+        user = uRepository.findByEmail(request.getEmail()).orElse(null);
+        if(user != null) throw new EmailAlreadyExistsException(request.getEmail());
+    }
+}
