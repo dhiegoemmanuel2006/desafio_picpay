4b0b6175d27556f6dc3473be887ac49a1eee646a
dhiego <dhiego12052006@gmail.com>
2025-04-08 01:11:07 -0300

    update: adicionando getters e setter por conta do erro do lombok

--- src/main/java/desafio_backend_picpay/desafio/handler/CustomErrorResponse.java
+++ src/main/java/desafio_backend_picpay/desafio/handler/CustomErrorResponse.java
@@ -11,4 +11,28 @@ public class CustomErrorResponse {
     private int status;
     private String error;
     private String code;
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getError() {
+        return error;
+    }
+
+    public void setError(String error) {
+        this.error = error;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
 }
