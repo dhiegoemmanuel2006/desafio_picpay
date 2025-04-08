1bc81177211f708b5e0d24bca1eee6c2188d6204
dhiego <dhiego12052006@gmail.com>
2025-04-07 23:10:53 -0300

    feat: Entidade principal do banco de dados: Users

---
+++ src/main/java/desafio_backend_picpay/desafio/database/entities/Users.java
@@ -0,0 +1,41 @@
+package desafio_backend_picpay.desafio.database.entities;
+
+import desafio_backend_picpay.desafio.database.enuns.TypeUser;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class Users {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_user")
+    private long id;
+
+    @Column(name = "name_user")
+    private String name;
+
+    @Column(name = "email_user", unique = true)
+    private String email;
+
+    @Column(name = "cpf_or_cnp_user", unique = true)
+    private String CpfOrCnpj;
+
+    @Column(name = "password_user")
+    private String password;
+
+    @Column(name = "type_user")
+    private TypeUser type;
+
+    @Column(name = "balance_user")
+    private BigDecimal balance;
+
+}
