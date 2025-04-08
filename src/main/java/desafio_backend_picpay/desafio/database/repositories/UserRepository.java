1170249d6251952b5de38b38b91b5c1de1d9a371
dhiego <dhiego12052006@gmail.com>
2025-04-08 00:27:29 -0300

    update: adicionando métodos na interface para conseguir fazer buscas personalizadas.

--- src/main/java/desafio_backend_picpay/desafio/database/repositories/UserRepository.java
+++ src/main/java/desafio_backend_picpay/desafio/database/repositories/UserRepository.java
@@ -3,5 +3,10 @@ package desafio_backend_picpay.desafio.database.repositories;
 import desafio_backend_picpay.desafio.database.entities.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<Users, Long> {
+    Optional<Users> findByCpfOrCnpj(String cpfOrCnpj);
+
+    Optional<Users> findByEmail(String email);
 }
